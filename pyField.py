6af1954e2a928de4ca7a71cb2d0a4061bf1db45e
harrington.ricks@live.com <harrington.ricks@live.com>
2021-08-12 07:26:37 -0400

    commiting changes

--- pyField.py
+++ pyField.py
@@ -1,10 +1,11 @@
 import ruamel.yaml
 import json
 in_file = '/jobs/rembrandt-mayan-edkii.yaml'
-out_file = 'output.json'
+out_file = '/.github/workflows/pyField.yaml'
 
 yaml = ruamel.yaml.YAML(typ='safe')
 with open(in_file) as fpi:
     data = yaml.load(fpi)
+    print(data)
 with open(out_file, 'w') as fpo:
     json.dump(data, fpo, indent=2)
