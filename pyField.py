f973823f162945b6ad280ccad4b5c6ff1f6d1b71
harrington.ricks@live.com <harrington.ricks@live.com>
2021-08-12 11:15:05 -0400

    changes import statement for module

--- pyField.py
+++ pyField.py
@@ -1,12 +1,9 @@
-import sys
-from ruamel.yaml import YAML
-import json
-in_file = '/jobs/rembrandt-mayan-edkii.yaml'
-out_file = '/.github/workflows/pyField.yaml'
 
-yaml = YAML(typ='safe')
-with open(in_file) as fpi:
-    data = yaml.load(fpi)
+#!/usr/bin/env python3
+
+import yaml
+
+with open('items.yaml') as f:
+    
+    data = yaml.load(f, Loader=yaml.FullLoader)
     print(data)
-with open(out_file, 'w') as fpo:
-    json.dump(data, fpo, indent=2)
